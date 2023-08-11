57690602ffc13618ed158da2455a6c4ac151cb7d
hanyongjun <gksdydwns34@naver.com>
2023-08-12 02:00:36 +0900

    or and 차이점 & 나이 계산기

--- app.js
+++ app.js
@@ -4,24 +4,30 @@
 // null -> 변수에 아무것도 없다는 것을 뜻함(자연적으로 발생하는 값이 아님)
 // undefined -> 정의되지 않음(변수가 메모리상엔 있지만 정의되진 않음)
 // function -> 하나의 함수 즉 여러 코드에서 중복으로 사용할 수 있는 조각같은 부분
+// const age = parseInt(prompt("How old are you?")); <-number로 받음
 
-const calculator = {
-    add : function (a,b){
-        console.log(a+b);
-    },
-    mius : function (a,b) {
-        console.log(a-b);
-    },
-    div: function(a,b) {
-        console.log(a/b);
-    } ,
-    powerof: function(a,b) {
-        console.log(a**b);
-    }
+const age = parseInt(prompt("How old are you?"));
+
+true || true === true
+false || true === true
+true || false === true
+false || false === false
+
+true && true === true
+false && true === false
+true && false === false
+false && false === false
+
+if(isNaN(age) || age < 0 ){
+    console.log("Please write a real positive number");
+} else if(age < 18) {
+    console.log("You are too young.");
+} else if (age >=18 && age <= 50) {
+    console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise");
+} else if (age > 80) {
+    console.log("You con do whatever you want.");
 }
 
-calculator.add(1,2);
-calculator.mius(4,2);
-calculator.div(5,1);
-calculator.powerof(1,2);
 
